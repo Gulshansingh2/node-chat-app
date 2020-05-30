11599c70546f4d22e8663b2044bdb73ce9012760
gulshan singh <gulshan.singh@bigbasket.com>
2020-05-30 14:19:35 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# chat-app
+Basic socket based node js chat app
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+
+//testing actions
+const app = express();
+const server = http.Server(app);
+// const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static('client'));
+
+server.listen(PORT, function() {
+  console.log('Node Server up');
+});
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  socket.on('message', function(msg) {
+    io.emit('message', msg);
+  });
+});
