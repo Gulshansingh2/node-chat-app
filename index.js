3b1fa533fa260254e5b629c91ec544d3f2b43f87
gulshan singh <gulshan.singh@bigbasket.com>
2020-05-31 12:18:03 +0530

    temp commit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const http = require('http');
 //testing actions
 const app = express();
 const server = http.Server(app);
-// const PORT = 8080;
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
+// const PORT = process.env.PORT || 8080;
 
 app.use(express.static('client'));
 
@@ -13,7 +13,7 @@ server.listen(PORT, function() {
   console.log('Node Server up');
 });
 
-var io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
 io.on('connection', function(socket) {
   socket.on('message', function(msg) {
